b83cb3ed42897bab3a823fa633717f55a6341c94
riajames <riajames.17@gmail.com>
2024-10-31 13:09:41 -0500

    banner image on home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import "../styles/Home.css"
+import banner from '../assets/home-banner.png';
 
 export const Home = () => {
     return (
         <div className={"home-container"}>
-            <h1>Home</h1>
+            <img className={"banner-image"} src={banner} alt={"banner for home page"}/>
         </div>
     );
 };
