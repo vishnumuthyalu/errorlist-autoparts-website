f77171e5c67e94503f12dcaf326509ec38d2c8ca
Chaubrich <connorhaubrich@outlook.com>
2024-11-05 11:12:51 -0600

    Added backend code to Category.js for sorting by price ascending/descending, and sorting alphabetically.
    Added code to firebase.js to hopefully fix the authentication as well as sign in/out features.

---
+++ src/Category.js
@@ -0,0 +1,12 @@
+//sort by price ascending
+const sortedByPrice = autoParts.sort((a, b) => a.price - b.price);
+console.log(sortedByPrice);
+
+//sort by price descending
+const sortedByPriceDesc = autoParts.sort((a, b) => b.price - a.price);
+console.log(sortedByPriceDesc);
+
+//Sort by Alphabetically
+const sortedByCategory = autoParts.sort((a, b) => a.category.localeCompare(b.category));
+console.log(sortedByCategory);
+
